93f83117b333da6b750fc45196c11a172d9f4b6b
Przemysław Czajka <zamaskowany@o2.pl>
2022-10-02 16:21:20 +0200

    Dodatkowa definicja

---
+++ def_func.py
@@ -0,0 +1,7 @@
+
+
+def dzielenie(a, b):
+    if b == 0:
+        print("Pamiętaj cholero, nie dziel przez zero")
+    else:
+        print("Twój wynik dzielenia to: ", (a/b))
--- zadanie42.py
+++ zadanie42.py
@@ -1,6 +1,6 @@
 #Kalkulator
 
-#import sys
+import def_func
 import logging
 from enum import IntEnum
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
@@ -14,9 +14,17 @@ wybor = int(input("""Wybierz ktore działanie chcesz wykonać wpisując liczbę:
 4. Dzielenie
 """))
 
+
+
 if wybor == menu_dzialania.Dodawanie:
-    a = int(input("Podaj pierwszą liczbę: "))
-    b = int(input("Podaj drugą liczbę: "))
+    try:
+        a = int(input("Podaj pierwszą liczbę: "))
+    except ValueError:
+        print("Miała być liczba!")
+    try:
+        b = int(input("Podaj drugą liczbę: "))
+    except ValueError:
+        print("Miała być liczba!")
     c = a + b
     logging.info("Dodajemy %s do %s" % (a, b))
     print("Wynik dodawania to: ", (c))
@@ -29,18 +37,13 @@ elif wybor == menu_dzialania.Odejmowanie:
 elif wybor == menu_dzialania.Mnożenie:
     a = int(input("Podaj pierwszą liczbę: "))
     b = int(input("Podaj drugą liczbę: "))
-    c = a * b
     logging.info("Mnożymy %s przez %s" % (a, b))
-    print("Wynik mnożenie to: ",(c))
+    print("Wynik mnożenia to: ", (a*b))
 elif wybor == menu_dzialania.Dzielenie:
     a = int(input("Podaj pierwszą liczbę: "))
     b = int(input("Podaj drugą liczbę: "))
-    if b == 0:
-        print("Pamiętaj cholero, nie dziel przez zero")
-
-    else:
-        c = a / b
+    def_func.dzielenie(a, b)
     logging.info("Dzielimy %s przez %s" % (a, b))
-    print("Wynik dzielenia to: ",(c))
 else:
     print("Niewłaściwy wybór!")
+    exit(1)
