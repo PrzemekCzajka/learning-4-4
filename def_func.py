6b778a938322ec9fd9e5ccef226741cbdc3fc027
Przemysław Czajka <zamaskowany@o2.pl>
2022-10-02 17:00:19 +0200

    change def

--- def_func.py
+++ def_func.py
@@ -4,4 +4,4 @@ def dzielenie(a, b):
     if b == 0:
         print("Pamiętaj cholero, nie dziel przez zero")
     else:
-        print("Twój wynik dzielenia to: ", (a/b))
+        print("Twój wynik dzielenia to: ", round(a/b, 2))
